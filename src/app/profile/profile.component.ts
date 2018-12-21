f351fad2eb9c6f1a929d9b3902088c81a2583ec6
karinasara-it1 <32881464+karinasara-it1@users.noreply.github.com>
2018-12-21 17:36:05 -0300

    Ajustes varios

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 import {SessionService} from '../services/session.service';
 import {UserService} from '../services/user.service';
 import {Post} from '../dtos/Post';
+import {ToolbarService} from '../services/toolbar.service';
 
 @Component({
   selector: 'app-profile',
@@ -17,6 +18,7 @@ export class ProfileComponent implements OnInit {
 
   constructor(private router: Router,
               private sessionService: SessionService,
+              private toolbarService: ToolbarService,
               private userService: UserService) { }
 
   ngOnInit() {
@@ -24,7 +26,8 @@ export class ProfileComponent implements OnInit {
       this.router.navigate(['/login']).then();
     } else {
       this.initializeUser();
-      this.sessionService.loadingPage('home');
+      this.toolbarService.addBasicToolbar('profile', 'Perfil de usuario');
+      this.sessionService.loadingPage('profile');
       // localStorage.setItem("page","home");
       this.getPosts();
     }
